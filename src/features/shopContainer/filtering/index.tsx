a44d2156fecf1458cacdc2070bdb288dbedfc673
Hayk Abrahamyan <abrahamyanhayk01@gmail.com>
2023-06-08 01:51:59 +0400

    Added soring fitering

--- src/features/shopContainer/filtering/index.tsx
+++ src/features/shopContainer/filtering/index.tsx
@@ -1,10 +1,21 @@
 import GridSvgIcon from '../../../icons/GridSvgIcon';
 import HamburgerSvgIcon from '../../../icons/HamburgerSvgIcon';
+import { useCallback, useRef } from 'react';
+import useChangeRouter from '../../../hooks/useChangeRouter';
 import cn from 'classnames';
 
 import styles from './styles.module.scss';
 
 const Filtering = (): JSX.Element => {
+  const { changeRouter } = useChangeRouter();
+
+  const handleChange = useCallback(
+    (value: string, key: string) => {
+      changeRouter([value] as string[], key);
+    },
+    [changeRouter]
+  );
+
   return (
     <div className="category-page-wrapper mb_30">
       <div
@@ -18,7 +29,11 @@ const Filtering = (): JSX.Element => {
           Show :
         </label>
         <div className="limit">
-          <select id="input-limit" className="form-control">
+          <select
+            id="input-limit"
+            className="form-control"
+            onChange={(event) => handleChange(event.target.value, 'limit')}
+          >
             <option value={8}>08</option>
             <option value={25}>25</option>
             <option value={50}>50</option>
@@ -35,16 +50,14 @@ const Filtering = (): JSX.Element => {
           Sort By :
         </label>
         <div className="sort-inner">
-          <select id="input-sort" className="form-control">
-            <option value="ASC">Default</option>
-            <option value="ASC">Name (A - Z)</option>
-            <option value="DESC">Name (Z - A)</option>
-            <option value="ASC">Price (Low &gt; High)</option>
-            <option value="DESC">Price (High &gt; Low)</option>
-            <option value="DESC">Rating (Highest)</option>
-            <option value="ASC">Rating (Lowest)</option>
-            <option value="ASC">Model (A - Z)</option>
-            <option value="DESC">Model (Z - A)</option>
+          <select
+            id="input-sort"
+            className="form-control"
+            onChange={(event) => handleChange(event.target.value, 'sort')}
+          >
+            <option value="asc">Default</option>
+            <option value="asc">Price (Low &gt; High)</option>
+            <option value="desc">Price (High &gt; Low)</option>
           </select>
         </div>
         <span>
